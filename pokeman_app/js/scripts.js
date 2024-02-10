d3cb774188b0fe70aa7b02858ad74100f54a2d00
SoyaKiss <orsolya.kiss.design@gmail.com>
2024-02-10 00:03:17 -0500

    test - delete repository; salvaged work from GitHub download

---
+++ pokeman_app/js/scripts.js
@@ -0,0 +1,6 @@
+let pokemonList = [
+    {name: 'Horsea', height: 4, type: ['damp','swim']},
+    {name: 'Porygon2', height: 6, type: ['trace','analytic']},
+    {name: 'Uxie', height: 3, type: ['levitate','psychic']},
+];
+
