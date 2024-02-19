0756055f7c68a37edd6419b7de62ee9e5f67e519
SoyaKiss <orsolya.kiss.design@gmail.com>
2024-02-19 12:27:42 -0500

    changed for -> forEach loop

--- pokeman_app/js/scripts.js
+++ pokeman_app/js/scripts.js
@@ -1,16 +1,60 @@
-let pokemonList = [
-    {name: 'Horsea', height: 4, type: ['damp', 'swim'] },
-    {name: 'Porygon2', height: 6, type: ['trace', 'analytic'] },
-    {name: 'Uxie', height: 3, type: ['levitate', 'psychic'] },
-];
+// This is the old for loop code
+// let pokemonList = [
+//     {name: 'Horsea', height: 4, type: ['damp', 'swim'] },
+//     {name: 'Porygon2', height: 6, type: ['trace', 'analytic'] },
+//     {name: 'Uxie', height: 3, type: ['levitate', 'psychic'] },
+// ];
 
-// This is where we created the loop that iterates over each item from my list; which gets sent over to the DOM via document write
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+// for (let i = 0; i < pokemonList.length; i++) {
+//     document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
 
-    if (pokemonList[i].height <= 3){
-        document.write(' - Wow, that\'s big!');
+//     if (pokemonList[i].height <= 3){
+//         document.write(' - Wow, that\'s big!');
+//     }
+//     document.write('<br />'); 
+// }
+
+
+// This is the new forEach loop
+// let pokemonList = [
+//     {name: 'Horsea', height: 4, type: ['damp', 'swim'] },
+//     {name: 'Porygon2', height: 6, type: ['trace', 'analytic'] },
+//     {name: 'Uxie', height: 3, type: ['levitate', 'psychic'] },
+// ];
+
+// pokemonList.forEach(function(pokemon) {
+//     document.write('<p>' + pokemon.name + " " + pokemon.height + " " + pokemon.type +  '<\p>');
+// });
+
+
+// This is putting the new code in an I.I.F.E
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name: 'Horsea', height: 4, type: ['damp', 'swim'] },
+        {name: 'Porygon2', height: 6, type: ['trace', 'analytic'] },
+        {name: 'Uxie', height: 3, type: ['levitate', 'psychic'] },
+    ];
+
+    function add (pokemon) {
+        pokemonList.push(pokemon);
     }
-    document.write('<br />'); 
-}
 
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    }
+    })();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Pika'});
+console.log(pokemonRepository.getAll());
+
+
+
+// pokemonList.forEach(function() {
+//     document.write('<p>' + pokemon.name + " " + pokemon.height + " " + pokemon.type +  '<\p>');
+// });
