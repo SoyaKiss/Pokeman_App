e2e39388735816329aa33cac158e5abd9413f620
SoyaKiss <orsolya.kiss.design@gmail.com>
2024-03-04 12:36:48 -0500

    New fonts; media-queries, event-handler(click)

--- pokeman_app/js/scripts.js
+++ pokeman_app/js/scripts.js
@@ -1,30 +1,3 @@
-// This is the old for loop code
-// let pokemonList = [
-//     {name: 'Horsea', height: 4, type: ['damp', 'swim'] },
-//     {name: 'Porygon2', height: 6, type: ['trace', 'analytic'] },
-//     {name: 'Uxie', height: 3, type: ['levitate', 'psychic'] },
-// ];
-
-// for (let i = 0; i < pokemonList.length; i++) {
-//     document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
-
-//     if (pokemonList[i].height <= 3){
-//         document.write(' - Wow, that\'s big!');
-//     }
-//     document.write('<br />'); 
-// }
-
-// This is the new forEach loop
-// let pokemonList = [
-//     {name: 'Horsea', height: 4, type: ['damp', 'swim'] },
-//     {name: 'Porygon2', height: 6, type: ['trace', 'analytic'] },
-//     {name: 'Uxie', height: 3, type: ['levitate', 'psychic'] },
-// ];
-
-// pokemonList.forEach(function(pokemon) {
-//     document.write('<p>' + pokemon.name + " " + pokemon.height + " " + pokemon.type +  '<\p>');
-// });
-
 // //This is putting the new code in an I.I.F.E
 let pokemonRepository = (function () {
     let pokemonList = [
@@ -41,9 +14,31 @@ let pokemonRepository = (function () {
         return pokemonList;
     }
 
+    function addListItem(pokemon) {
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+
+        listItem.classList.add('list-item-pokemon');
+        button.innerText = pokemon.name;
+        button.classList.add('pokemon-button');
+
+        listItem.appendChild(button);
+        pokemonUnorderedList.appendChild(listItem);
+        
+        pokemon-button.addEventListener('click', function() {
+            showDetails(pokemon);
+        })
+    }
+
+    function showDetails(pokemon) {
+        console.log(pokemon.name)
+    };
+    
+
     return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListItem: addListItem,
     }
     })();
 
@@ -51,9 +46,13 @@ console.log(pokemonRepository.getAll());
 pokemonRepository.add({ name: 'Pikachu', height: 8, type: ['cute', 'adorable'] });
 console.log(pokemonRepository.getAll());
 
+let pokemonUnorderedList = document.querySelector('.pokemon-list');
+
 pokemonRepository.getAll().forEach(function(pokemon) {
-    document.write('<p><ul><li>Name: ' + pokemon.name + '</li><li>' + 
-                    'Height: ' + pokemon.height + '</li><li>' + 
-                    'Type: ' + pokemon.type[0] + ' + ' + pokemon.type[1] + 
-                    '</li></ul><\p>');
+    pokemonRepository.addListItem(pokemon);
 });
+
+// InnerText - assigning a header title
+let mainTitle = document.querySelector('h1');
+mainTitle.innerText = 'Pokedex';
+mainTitle.classList.add('pokedex-title');
