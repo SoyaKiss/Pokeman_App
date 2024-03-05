04047267c55bdea94cbd957a25e4d2bbeaa01e1d
SoyaKiss <orsolya.kiss.design@gmail.com>
2024-03-04 22:14:26 -0500

    added polyfills, loads pokemon data from API; addListItem and showDetails functions in JS

--- pokeman_app/js/scripts.js
+++ pokeman_app/js/scripts.js
@@ -1,10 +1,6 @@
-// //This is putting the new code in an I.I.F.E
 let pokemonRepository = (function () {
-    let pokemonList = [
-        {name: 'Horsea', height: 4, type: ['damp', 'swim'] },
-        {name: 'Porygon2', height: 6, type: ['trace', 'analytic'] },
-        {name: 'Uxie', height: 3, type: ['levitate', 'psychic'] },
-    ];
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function add (pokemon) {
         pokemonList.push(pokemon);
@@ -15,43 +11,71 @@ let pokemonRepository = (function () {
     }
 
     function addListItem(pokemon) {
+        let pokemonUnorderedList = document.querySelector('.pokemon-list');
         let listItem = document.createElement('li');
         let button = document.createElement('button');
-
         listItem.classList.add('list-item-pokemon');
         button.innerText = pokemon.name;
         button.classList.add('pokemon-button');
-
         listItem.appendChild(button);
         pokemonUnorderedList.appendChild(listItem);
-        
-        pokemon-button.addEventListener('click', function() {
+        pokemon-button.addEventListener('click', function(event) {
             showDetails(pokemon);
+        });
+    }
+
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+          return response.json();
+        }).then(function (json) {
+          json.results.forEach(function (item) {
+            let pokemon = {
+              name: item.name,
+              detailsUrl: item.url
+            };
+            add(pokemon);
+            console.log(pokemon);
+          });
+        }).catch(function (e) {
+          console.error(e);
         })
     }
 
-    function showDetails(pokemon) {
-        console.log(pokemon.name)
-    };
-    
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+          return response.json();
+        }).then(function (details) {
+          item.imageUrl = details.sprites.front_default;
+          item.height = details.height;
+          item.types = details.types;
+        }).catch(function (e) {
+          console.error(e);
+        });
+    }
 
+    function showDetails(item) {
+        pokemonRepository.loadDetails(item).then(function () {
+          console.log(item);
+        });
+    }
+    
     return {
         add: add,
         getAll: getAll,
         addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails,
+        showDetails: showDetails
     }
     })();
 
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Pikachu', height: 8, type: ['cute', 'adorable'] });
-console.log(pokemonRepository.getAll());
-
-let pokemonUnorderedList = document.querySelector('.pokemon-list');
-
-pokemonRepository.getAll().forEach(function(pokemon) {
-    pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+    pokemonRepository.getAll().forEach(function (pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
 });
-
+      
 // InnerText - assigning a header title
 let mainTitle = document.querySelector('h1');
 mainTitle.innerText = 'Pokedex';
